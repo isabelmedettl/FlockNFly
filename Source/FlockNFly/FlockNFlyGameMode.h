e661d9cd80951f8a5f1c84cb2e9561f1ff4543bb
isabelmedettl <89594987+isabelmedettl@users.noreply.github.com>
2023-07-23 08:38:25 +0200

    Initial commit

---
+++ Source/FlockNFly/FlockNFlyGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FlockNFlyGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFlockNFlyGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFlockNFlyGameMode();
+};
+
+
+
