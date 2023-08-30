3c94ff81b283e12c62eb65e70faf94bda8df2f96
Isabel <isabel.d.johansson@gmail.com>
2023-08-30 15:57:26 +0200

    Bits

--- Source/FlockNFly/GeometryGenerator.cpp
+++ Source/FlockNFly/GeometryGenerator.cpp
@@ -17,7 +17,8 @@ void AGeometryGenerator::BeginPlay()
 {
 	Super::BeginPlay();
 
-	
+	BitArray = ConvertSeedToBit();
+	GenerateGrid();
 	
 }
 
@@ -25,25 +26,46 @@ void AGeometryGenerator::BeginPlay()
 void AGeometryGenerator::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	FString NewSeed = ConvertSeedToBit();
-	UE_LOG(LogTemp, Warning, TEXT("bit ,%s"), *NewSeed);
+	
+	//UE_LOG(LogTemp, Warning, TEXT("bit ,%s"), *NewSeed);
 }
 
-FString AGeometryGenerator::ConvertSeedToBit()
+TArray<uint8> AGeometryGenerator::ConvertSeedToBit()
 {
-	FString BinaryString;
+	TArray<uint8> BinaryString;
 
-	for (uint64 i = 0; i < Seed.Len(); ++i)
+	for (uint64 x = 0; x < Seed.Len(); ++x)
 	{
-		uint8 ASCIIvalue = static_cast<uint8>(Seed[i]);
-		FString CharBinary;
-		for (int32 BitIndex = 7; BitIndex >=0; --BitIndex)
-		{
-			bool Bit = (ASCIIvalue >> BitIndex) & 1;
-			CharBinary += Bit ? TEXT("1") : TEXT("0");
-		}
-		BinaryString += CharBinary;
+		uint8 ASCIIvalue = static_cast<uint8>(Seed[x]);
+		BinaryString.Add(ASCIIvalue);
+		
 	}
 	return BinaryString;
+}
+
+namespace BitFuncs
+{
+	uint8 GetBitValueAtIndex(TArray<uint8> &IntArray, int Index)
+	{
+		int BitIndex = Index / 8;
+		int Byte = IntArray[BitIndex];
+		return Byte >> (Index % 8) & 1U; 
+	}
+	
+}
+
+void AGeometryGenerator::GenerateGrid()
+{
+	for (int x = 0; x < Grid.GridX; x++)
+	{
+		for (int y = 0; y <Grid.GridY; y++)
+		{
+			for (int z = 0; z < Grid.GridZ; z++)
+			{
+				Grid.IntGrid[x][y][z] = BitFuncs::GetBitValueAtIndex(BitArray, x + y + z);
+				UE_LOG(LogTemp, Warning, TEXT("bit ,%ull"), Grid.IntGrid[x][y][z]);
+			}
+		}
+	}
 }
 
--- Source/FlockNFly/GeometryGenerator.h
+++ Source/FlockNFly/GeometryGenerator.h
@@ -6,6 +6,33 @@
 #include "GameFramework/Actor.h"
 #include "GeometryGenerator.generated.h"
 
+
+USTRUCT()
+struct FGrid
+{
+	GENERATED_BODY()
+
+	FGrid()
+	{
+		GridX = 0.f;
+		GridY = 0.f;
+		GridZ = 0.f;
+	}
+
+	UPROPERTY(EditAnywhere, Category="Grid")
+	int GridX = 0.f;
+
+	UPROPERTY(EditAnywhere, Category="Grid")
+	int GridY = 0.f;
+
+	UPROPERTY(EditAnywhere, Category="Grid")
+	int GridZ = 0.f;
+	
+	TArray<TArray<TArray<int>>> IntGrid = TArray<TArray<TArray<int>>>();
+
+};
+
+
 UCLASS()
 class FLOCKNFLY_API AGeometryGenerator : public AActor
 {
@@ -15,16 +42,6 @@ public:
 	// Sets default values for this actor's properties
 	AGeometryGenerator();
 
-	UPROPERTY(EditAnywhere)
-	float GridX = 0.f;;
-
-	UPROPERTY(EditAnywhere)
-	float GridY = 0.f;
-
-	UPROPERTY(EditAnywhere)
-	float GridZ = 0.f;
-
-	
 
 protected:
 	// Called when the game starts or when spawned
@@ -33,12 +50,20 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+	
 
-
+	UPROPERTY(EditAnywhere)
+	FGrid Grid;
+	
 private:
 	FString Seed = "u45on30b56qewiurq34qv934e";
 
+	
 
-	FString ConvertSeedToBit();
+	TArray<uint8> BitArray = TArray<uint8>();
+
+	TArray<uint8> ConvertSeedToBit();
+
+	void GenerateGrid();
 
 };
